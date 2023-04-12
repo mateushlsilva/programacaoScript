49e3c908142441511c7b44d1d33b16c3865d8df9
mateushlsilva <mateushls01@gmail.com>
2023-04-12 15:52:18 -0300

    Adiciona menu

--- atvi2/loteria/src/routes/index.tsx
+++ atvi2/loteria/src/routes/index.tsx
@@ -1,16 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Quina from '../pages/Quina';
 import Lotofacil from '../pages/Lotofacil';
 import Megasena from '../pages/Megasena';
+
+
+function Erro() {
+    return(
+        <div style={{textAlign:'center', fontSize: '30px', marginTop: '300px'}}>Indisponível ...</div>
+    )
+}
+
+function Menu() {
+    return(
+        <div>
+            <Link to={"/lotofacil"}> Lotofácil</Link>
+            <Link to={"/megasena"}> Megasena</Link>
+            <Link to={"/quina"}> Quina</Link>
+        </div>
+    )
+}
 
 function Rota() {
     return (
         <>
             <BrowserRouter>
+            <Menu/>
                 <Routes>
                     <Route path='/quina' element={<Quina/>}/>
                     <Route path='/lotofacil' element={<Lotofacil/>}/>
                     <Route path='/megasena' element={<Megasena/>}/>
+                    <Route path='/' element={<Lotofacil/>}/>
+                    <Route path='*' element={<Erro/>}/>
                 </Routes>
             </BrowserRouter>
         </>
