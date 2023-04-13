1ff09eef50aae8c88ccf5867a3befa560a8e4189
mateushlsilva <mateushls01@gmail.com>
2023-04-13 14:43:01 -0300

    arruma paginas

--- atvi1/loteria/src/App.tsx
+++ atvi1/loteria/src/App.tsx
@@ -1,12 +1,30 @@
 import { Provider } from "./contexts";
 import Principal from "./pages/Principal";
 import './App.css'
+import { useContexto } from "./hooks";
+
+function Carregando() {
+  const {megasena} = useContexto()
+  return (
+    <>
+      {megasena.numeroDoConcurso ?
+        <div>
+          <Principal />
+        </div>
+        :
+        <div style={{textAlign:'center', fontSize: '30px', marginTop: '300px'}}>Carregando....</div>}
+
+    </>
+  )
+}
+
+
 
 function App() {
 
   return (
     <Provider>
-      <Principal />
+      <Carregando />
     </Provider>
   )
 }
--- atvi1/loteria/src/pages/Principal.tsx
+++ atvi1/loteria/src/pages/Principal.tsx
@@ -1,17 +1,61 @@
-import Home from "../components/home/Home";
-import { useContexto } from "../hooks";
+import trevoMega from '../assets/trevo-megasena.png'
+import trevoLoto from '../assets/trevo-lotofacil.png'
+import useContexto from '../hooks/useContexto';
+import Ganhadores from '../components/ganhadores/Ganhadores';
+import InforConcurso from '../components/infoConcurso/InforConcurso';
+import ProximoSorteio from '../components/infoProximoSorteio/ProximoSorteio';
+import NumerosSorteados from '../components/numerosSorteados/NumerosSorteados';
+import NumerosSorteadosLoto from '../components/numerosSorteados/NumerosSorteadosLoto';
+import Titulo from '../components/titulo/titulo';
+import TituloLoto from '../components/titulo/tituloLoto';
+import Valor from '../components/valor/valor';
+import ValorLoto from '../components/valor/valorLoto';
+export default function Megasena() {
 
-export default function Principal() {
   const {megasena} = useContexto()
-  return (
-    <>
-      {megasena.numeroDoConcurso ?
-        <div>
-          <Home />
-        </div>
-        :
-        <div>Carregando....</div>}
+  const {lotofacil} = useContexto()
 
-    </>
+  return (
+    <div>
+    <div className='cont'>
+
+      <div className='direito'>
+
+        <div className='trevo'><img src={trevoMega} /><Titulo titulo={"Mega-sena"} /> </div>
+        <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${megasena.dataProximoConcurso}:`} />
+        <Valor valor={megasena.valorEstimadoProximoConcurso} />
+
+      </div>
+
+      <div className='esquerdo'>
+
+        <NumerosSorteados numeros={megasena.dezenas} />
+        <Ganhadores ganhadores={megasena.quantidadeGanhadores ? `${megasena.quantidadeGanhadores} GANHADORES` : "ACUMULOU"} />
+        <InforConcurso informacoes={`Concuso: ${megasena.numeroDoConcurso} -`} data={` ${megasena.dataPorExtenso}`} />
+
+      </div>
+
+    </div>
+
+    <div className='cont'>
+
+      <div className='direito'>
+
+        <div className='trevo'><img src={trevoLoto} /><TituloLoto titulo={"Loto facil"} /></div>
+        <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${lotofacil.dataProximoConcurso}:`} />
+        <ValorLoto valor={lotofacil.valorEstimadoProximoConcurso} />
+
+      </div>
+      <div className='esquerdo'>
+
+        <NumerosSorteadosLoto numeros={lotofacil.dezenas} />
+        <Ganhadores ganhadores={lotofacil.quantidadeGanhadores ? `${lotofacil.quantidadeGanhadores} GANHADORES` : "ACUMULOU"} />
+        <InforConcurso informacoes={`Concuso: ${lotofacil.numeroDoConcurso} -`} data={` ${lotofacil.dataPorExtenso}`} />
+
+      </div>
+
+    </div>
+  </div>
+
   )
 }
